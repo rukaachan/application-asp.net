29cc439302242e14752e4adab9eec2e33af65d22
taufik-fauzi <novops911@gmail.com>
2023-02-18 16:03:58 +0700

    fist commit

---
+++ InputAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InputAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
